cf049110bc1ca2aa7b4dde0549fc30356357569a
Ahmad Fauzan <ahmadfauzaan613@gmail.com>
2025-06-29 12:02:01 +0700

    chore: update next.config.ts for improved webpack watch options and image domains

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,16 @@
 import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
+  webpack: (config) => {
+    config.watchOptions = {
+      ignored: ['**/node_modules/**', '**/public/upload/**', '**/C:/Users/ahmad/Application Data/**', '**/C:/Users/ahmad/Cookies/**'],
+    }
+    return config
+  },
   images: {
     domains: ['localhost'],
   },
+  staticPageGenerationTimeout: 60,
 }
 
 export default nextConfig
